79d0332893c608eee179ce02b625ccb199dff499
almudenaredradix <almudena@redradix.com>
2024-01-19 13:49:02 +0100

    :lipstick: Add tablet layout to MainStoryGrid and SecondaryStory

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -19,7 +19,6 @@ const MainStoryGrid = () => {
       <MainStorySection>
         <MainStory {...MAIN_STORY} />
       </MainStorySection>
-
       <SecondaryStorySection>
         <StoryList>
           {SECONDARY_STORIES.map((story, index) => (
@@ -32,15 +31,14 @@ const MainStoryGrid = () => {
 
       <OpinionSection>
         <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
+        <OpinionStoryList>
           {OPINION_STORIES.map((story) => (
-            <VerticalStoryWrapper key={story.id}>
+            <OpinionStoryWrapper key={story.id}>
               <OpinionStory {...story} />
-            </VerticalStoryWrapper>
+            </OpinionStoryWrapper>
           ))}
-        </StoryList>
+        </OpinionStoryList>
       </OpinionSection>
-
       <AdvertisementSection>
         <Advertisement />
       </AdvertisementSection>
@@ -57,10 +55,25 @@ const Wrapper = styled.div`
     'advertisement';
   gap: 48px;
   margin-bottom: 48px;
+
+  @media ${props => props.theme.QUERIES.tabletAndUp} {
+    grid-template-areas:
+    'main-story secondary-stories'
+    'advertisement advertisement'
+    'opinion-stories opinion-stories';
+    grid-template-columns: 2fr 1fr;
+    gap: 48px 0;
+  }
 `;
 
 const MainStorySection = styled.section`
   grid-area: main-story;
+
+  @media ${props => props.theme.QUERIES.tabletAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
 `;
 
 const SecondaryStorySection = styled.section`
@@ -72,18 +85,36 @@ const StoryList = styled.div`
   flex-direction: column;
 `;
 
+const OpinionStoryList = styled(StoryList)`
+  @media ${props => props.theme.QUERIES.tabletOnly} {
+    flex-direction: row;
+    gap: 32px;
+  }
+`
+
 const VerticalStoryWrapper = styled.div`
-  &:not(:last-of-type){
+  flex: 1;
+  min-width: 0;
+
+  &:not(:last-of-type) {
     border-bottom: 1px solid var(--color-gray-300);
     padding-bottom: 1rem;
     margin-bottom: 1rem;
   }
 `
 
+const OpinionStoryWrapper = styled(VerticalStoryWrapper)`
+  @media ${props => props.theme.QUERIES.tabletOnly} {
+    &:not(:last-of-type){
+      border-bottom: revert;
+      padding-bottom: revert;
+      margin-bottom: revert;
+    }
+  }
+`
+
 const OpinionSection = styled.section`
   grid-area: opinion-stories;
-
-
 `;
 
 const AdvertisementSection = styled.section`
--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -21,6 +21,15 @@ const Wrapper = styled.article`
   gap: 4px 16px;
   grid-template-columns: 120px 1fr;
   color: var(--color-gray-900);
+  container-type: block-size;
+
+  @container (width <= 300px) {
+    grid-template-areas:
+    'image'
+    'heading'
+    'abstract';
+    grid-template-columns: 1fr;
+  }
 `;
 
 const Image = styled.img`
